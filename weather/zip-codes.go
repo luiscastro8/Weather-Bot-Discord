35320993851288bd938648984e6acb938f7174c8
luiscastro8 <ochoad64@gmail.com>
2022-09-18 11:21:54 -0500

    made functions to get coords from zip from file

---
+++ weather/zip-codes.go
@@ -0,0 +1,40 @@
+package weather
+
+import (
+	"encoding/csv"
+	"errors"
+	"os"
+)
+
+var zipCodes map[string][]string
+
+func getCoordsFromZip(zip string) (string, string, error) {
+	if zipCodes == nil {
+		return "", "", errors.New("zip code cache is empty")
+	}
+
+	coords, present := zipCodes[zip]
+	if !present {
+		return "", "", errors.New("zip code is not in cache")
+	}
+	return coords[0], coords[1], nil
+}
+
+func OpenZipFile() error {
+	open, err := os.Open("zip-codes.csv")
+	if err != nil {
+		return err
+	}
+
+	reader := csv.NewReader(open)
+	data, err := reader.ReadAll()
+	if err != nil {
+		return err
+	}
+
+	zipCodes = make(map[string][]string, len(data))
+	for _, val := range data {
+		zipCodes[val[0]] = []string{val[1], val[2]}
+	}
+	return nil
+}
