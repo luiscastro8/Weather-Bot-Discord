3669b2c8d5c71f508fcaa5cd646b2f1d238a3510
luiscastro8 <ochoad64@gmail.com>
2022-10-06 18:31:00 -0500

    added support for aws secrets

--- go.mod
+++ go.mod
@@ -3,12 +3,17 @@ module Weather-Bot-Discord
 go 1.19
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.16.16
+	github.com/aws/aws-sdk-go-v2/service/secretsmanager v1.16.2
 	github.com/bwmarrin/discordgo v0.26.1
 	github.com/fatih/color v1.13.0
 	github.com/stretchr/testify v1.8.0
 )
 
 require (
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.23 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.17 // indirect
+	github.com/aws/smithy-go v1.13.3 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.9 // indirect
--- token/token.go
+++ token/token.go
@@ -1,14 +1,45 @@
 package token
 
 import (
+	"context"
 	"errors"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
 	"os"
+	"strings"
 )
 
 func GetToken() (string, error) {
+	cloud := os.Getenv("CLOUD")
+	if strings.ToLower(cloud) == "aws" {
+		secretId := os.Getenv("AWS_SECRET_ID")
+		region := os.Getenv("AWS_REGION")
+		if secretId == "" || region == "" {
+			return "", errors.New("specified aws as cloud but aws secret id or aws region were not given")
+		}
+		return getTokenFromAws(secretId, region)
+	}
+
 	token := os.Getenv("DISCORD_TOKEN")
 	if token == "" {
 		return "", errors.New("DISCORD_TOKEN environment variable is empty")
 	}
 	return token, nil
+}
+
+func getTokenFromAws(secretId, region string) (string, error) {
+	client := secretsmanager.New(secretsmanager.Options{Region: region})
+
+	secretValueOutput, err := client.GetSecretValue(context.Background(), &secretsmanager.GetSecretValueInput{
+		SecretId: aws.String(secretId),
+	})
+	if err != nil {
+		return "", err
+	}
+
+	if secretValueOutput.SecretString == nil {
+		return "", errors.New("aws secret value is empty")
+	}
+
+	return *secretValueOutput.SecretString, nil
 }
