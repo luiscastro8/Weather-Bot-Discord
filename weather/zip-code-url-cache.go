3a8f1a736bc3cae9b9f96aee130c98bcad52edaa
luiscastro8 <ochoad64@gmail.com>
2022-10-21 15:31:20 -0500

    added cache

---
+++ weather/zip-code-url-cache.go
@@ -0,0 +1,26 @@
+package weather
+
+import "sync"
+
+var zipCodesToUrl map[string]string
+var lock sync.RWMutex
+
+func GetUrlFromCache(zip string) (string, bool) {
+	lock.RLock()
+	defer lock.RUnlock()
+	url, ok := zipCodesToUrl[zip]
+	return url, ok
+}
+
+func AcquireLockForCaching() {
+	lock.Lock()
+}
+
+func ReleaseLockForCaching() {
+	lock.Unlock()
+}
+
+func WriteToCache(zip, url string) {
+	zipCodesToUrl[zip] = url
+	lock.Unlock()
+}
