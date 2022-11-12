9942a414884550dde3cc7a277e22e0c69c1fc52b
luiscastro8 <ochoad64@gmail.com>
2022-11-12 17:52:15 -0600

    moved function

--- discord/discord.go
+++ discord/discord.go
@@ -2,9 +2,6 @@ package discord
 
 import (
 	"Weather-Bot-Discord/mylogger"
-	"Weather-Bot-Discord/weather/forecast"
-	"Weather-Bot-Discord/weather/points"
-	"fmt"
 	"github.com/bwmarrin/discordgo"
 )
 
@@ -16,33 +13,7 @@ func WeatherHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	} else if subCommand.Name == "coordinates" {
 		lat := subCommand.Options[0].FloatValue()
 		long := subCommand.Options[1].FloatValue()
-
-		forecastUrl, err := points.GetForecastURLFromCoords(fmt.Sprintf("%.4f", lat), fmt.Sprintf("%.4f", long))
-		if err != nil {
-			mylogger.Errorln(err)
-			err = sendSlashCommandResponse(s, i, "There was an error getting the forecast")
-			if err != nil {
-				mylogger.Errorln("could not send slash command message:", err)
-			}
-			return
-		}
-
-		forecastMessage, err := forecast.GetForecastFromURL(forecastUrl, "")
-		if err != nil {
-			mylogger.Errorln(err)
-			err = sendSlashCommandResponse(s, i, "There was an error getting the forecast")
-			if err != nil {
-				mylogger.Errorln("could not send slash command message:", err)
-			}
-			return
-		}
-
-		err = sendSlashCommandResponse(s, i, forecastMessage)
-		if err != nil {
-			mylogger.Errorln("could not send slash command message:", err)
-		} else {
-			mylogger.Println("sent weather forecast for coords", lat, long)
-		}
+		coordinatesHandler(s, i, lat, long)
 	} else if subCommand.Name == "address" {
 		address := subCommand.Options[0].StringValue()
 		addressHandler(s, i, address)
