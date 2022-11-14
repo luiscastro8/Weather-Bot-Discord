2f7f0a65d68ad98caf2d063dc70ec584bc06a634
luiscastro8 <ochoad64@gmail.com>
2022-11-13 19:08:39 -0600

    fix typos

--- myerrors/address-not-found_test.go
+++ myerrors/address-not-found_test.go
@@ -6,9 +6,9 @@ import (
 )
 
 func TestNewAddressNotFoundError(t *testing.T) {
-	t.Run("It should return error with correct message and unmatched address", func(t *testing.T) {
+	t.Run("It should return an error with the correct message and unmatched address", func(t *testing.T) {
 		testMessage := "this is a test message"
-		address := "12345678 fake street ln"
+		address := "12345678 fake street lane"
 		err := NewAddressNotFoundError(testMessage, address)
 		assert.Equal(t, testMessage, err.Error())
 		assert.Equal(t, address, err.UnmatchedAddress)
