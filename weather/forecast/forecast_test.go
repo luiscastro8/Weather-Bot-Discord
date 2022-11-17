f6615d2f0240b09bea7e40dcfacdbd29b7d57924
luiscastro8 <ochoad64@gmail.com>
2022-11-16 21:45:03 -0600

    added tests

--- weather/forecast/forecast_test.go
+++ weather/forecast/forecast_test.go
@@ -1,7 +1,9 @@
 package forecast
 
 import (
-	//"encoding/json"
+	"encoding/json"
+	"errors"
+
 	"github.com/stretchr/testify/assert"
 	"io"
 	"net/http"
@@ -11,13 +13,28 @@ import (
 
 func TestProcessResponse(t *testing.T) {
 	t.Run("It should return an error if the status code was not 2XX", func(t *testing.T) {
-		s, err := processResponse(createMockResponse("", 500), "")
+		mockResponse := createMockResponse("", 500)
+		s, err := processResponse(mockResponse, "")
 		assert.Equal(t, "", s)
 		assert.Error(t, err)
 	})
 
 	t.Run("It should return an error if the status code was not 2XX and the response has error details", func(t *testing.T) {
-		panic("this is not")
+		responseBody := createMockResponseErrorBody(errorResponse{Detail: "an expected error has occurred"})
+		mockResponse := createMockResponse(responseBody, 500)
+		s, err := processResponse(mockResponse, "")
+		assert.Equal(t, "", s)
+		assert.Error(t, err)
+	})
+
+	t.Run("It should return an error if unable to read from response body", func(t *testing.T) {
+		mockResponse := &http.Response{
+			StatusCode: 200,
+			Body:       io.NopCloser(errReader{}),
+		}
+		s, err := processResponse(mockResponse, "")
+		assert.Equal(t, "", s)
+		assert.Error(t, err)
 	})
 }
 
@@ -28,14 +45,25 @@ func createMockResponse(body string, code int) *http.Response {
 	}
 }
 
-func createMockResponseBody() string {
-	//bytes, err := json.Marshal()
-	//if err != nil {
-	//	return ""
-	//}
-	return ""
+func createMockResponseBody(r response) string {
+	bytes, err := json.Marshal(r)
+	if err != nil {
+		panic(err)
+	}
+	return string(bytes)
 }
 
-func createMockResponseBodyError(r errorResponse) string {
-	return ""
+func createMockResponseErrorBody(r errorResponse) string {
+	bytes, err := json.Marshal(r)
+	if err != nil {
+		panic(err)
+	}
+	return string(bytes)
+}
+
+type errReader struct{}
+
+func (e errReader) Read(p []byte) (int, error) {
+	p[0] = 'a'
+	return 0, errors.New("intentional error")
 }
